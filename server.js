21ce16712abe1afc3db94bbca4330f6332fb07f2
Manasa <hsvishwanth91@gmail.com>
2026-05-13 01:44:50 +0530

    fixed api route

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API working" });
 });
 
 const PORT = process.env.PORT || 3000;
